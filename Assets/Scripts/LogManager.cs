62bdec03cf579c8607586573c9a1d0c693e1756d
kjh3357 <bmj1644035@gmai.com>
2026-04-23 18:08:08 +0900

    로그매니저 스크립트 수정

--- Assets/Scripts/LogManager.cs
+++ Assets/Scripts/LogManager.cs
@@ -1,15 +1,41 @@
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogManager : MonoBehaviour
 {
-    public TMP_InputField inputField;
+    [SerializeField] private TextMeshProUGUI logText;
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private TMP_Dropdown dropdown2;
+    [SerializeField] private Button button;
+    [SerializeField] private Queue<string> logQueue = new Queue<string>();
 
-    private Queue<string> logQueue = new Queue<string>();
-
-    public  void sendText()
+    public void Start()
     {
+        dropdown.onValueChanged.AddListener(OnDropdownChanged);
+        dropdown2.onValueChanged.AddListener(OnDropdownChanged);
 
+        button.onClick.AddListener(OnButtonClicked);
+    }
+
+    public void OnDropdownChanged(int index)
+    {
+        string selected = dropdown.options[index].text;
+        sendText($"드롭다운 변경: {selected}");
+    }
+
+    public void OnButtonClicked()
+    {
+        sendText($"버튼 클릭됨");
+    }
+
+    public void sendText(string message)
+    {
+        logText.text += message + "\n";
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 0f;
     }
 }
