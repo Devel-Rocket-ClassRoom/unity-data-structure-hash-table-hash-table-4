a1b964c61a2ecf218436cefc8a3e9d588bf14b27
skatkdwns <skatkdwns08@gmail.com>
2026-04-24 11:18:36 +0900

    OpenAdressing

--- Assets/Scripts/HashTable.cs
+++ Assets/Scripts/HashTable.cs
@@ -5,10 +5,13 @@ public class HashTable<TKey,TValue>
     public TKey Key { get; set; }
     public TValue Value { get; set; }
     public bool IsOccupied {  get; set; }
+    public bool IsDeleted { get; set; }
     public HashTable<TKey, TValue> Next { get; set; }
     public HashTable(TKey key, TValue value)
     {
         this.Key = key;
         this.Value = value;
+        IsOccupied = false;
+        IsDeleted = false;
     }
 }
